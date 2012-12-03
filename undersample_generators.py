1bfd52a3ebf46bc03eaec31e7c9443a2b2f33547
Corey Lynch <coreylynch9@gmail.com>
2012-12-02 21:19:20 -0500

    r g

--- undersample_generators.py
+++
@@ -1,9 +0,0 @@
-# undersample on disk
-
-# use a generator to count y
-
-def count_y(filename):
-	with open(filename, 'r') as f:
-		y_column = (line.split(',')[-1] for line in f)
-		counter = collections.Counter((i.strip() for i in y_column))
-		return counter
